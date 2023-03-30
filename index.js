7ba094d40f94788f3faf949c2c55641423dfe55a
tgashwinkumar <tgashwinkumar@gmail.com>
2023-03-30 15:35:16 +0530

    Backend commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const express = require("express");
+const app = express();
+const port = 4050;
+const Gun = require("gun");
+
+app.use(Gun.serve);
+
+const server = app.listen(port, () => {
+  console.log(`Gun server running on port ${port}🔥`);
+});
+
+Gun({ web: server });
+
+module.exports = app;
