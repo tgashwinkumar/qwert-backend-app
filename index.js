fa02202cbe52702a9f219dbefbe965edb4562ab0
tgashwinkumar <tgashwinkumar@gmail.com>
2023-03-30 17:45:50 +0530

    ADded

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-
 const express = require("express");
 const app = express();
 const port = 4050;
 const Gun = require("gun");
 
 app.use(Gun.serve);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 const server = app.listen(port, () => {
   console.log(`Gun server running on port ${port}🔥`);
